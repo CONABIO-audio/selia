0c86cded42a274e055d59c188388455af16cb882
jdonlucas <jdonlucas93@gmail.com>
2021-01-11 13:20:33 -0600

    UPLOAD_APP:: adding credentials to post request

--- selia_uploader/app/services/api.js
+++ selia_uploader/app/services/api.js
@@ -2,7 +2,14 @@ import axios from 'axios';
 
 export default {
     validate(data) {
-        return axios.post('http://irekuaapi-env.eba-gj4jy7ue.us-west-2.elasticbeanstalk.com/api/collections/v1/collection_items/validate/',
-        data)
+        return axios({
+            method: 'post',
+            url: 'http://irekuaapi-env.eba-gj4jy7ue.us-west-2.elasticbeanstalk.com/api/collections/v1/collection_items/validate/',
+            data: data,
+            auth: {
+                username: 'jdonlucas',
+                password: 'jdonlucas'
+            }
+        })
     }
 }
