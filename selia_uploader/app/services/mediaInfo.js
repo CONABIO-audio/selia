fe2091a5129b1f2bd6ae244e5176c7694992a45a
mbsantiago <santiago.mbal@gmail.com>
2021-02-13 12:32:43 +0000

    changing the upload app view

---
+++ selia_uploader/app/services/mediaInfo.js
@@ -0,0 +1,106 @@
+import { getInfo } from "react-mediainfo";
+import exifr from "exifr";
+
+export default {
+  async getMediaInfo(file) {
+    if (file.type.includes("audio")) {
+      const mediaInfo = await getInfo(file);
+      try {
+        let metadata = mediaInfo.media.track[1];
+        let audioMediaInfo = {
+          frames: parseInt(metadata.SamplingCount),
+          channels: parseInt(metadata.Channels),
+          duration: parseFloat(metadata.Duration),
+          sampwidth: parseInt(metadata.BitDepth) / 8,
+          sampling_rate: parseInt(metadata.SamplingRate),
+        };
+        return audioMediaInfo;
+      } catch {
+        return {};
+      }
+    } else if (file.type.includes("image")) {
+      let exif = await exifr.parse(file);
+      let device = exif
+        ? exif.Make
+          ? exif.Make
+          : "No especificado"
+        : "No especificado";
+      let date = exif
+        ? exif.CreateDate
+          ? exif.CreateDate.toISOString()
+          : exif.ModifyDate
+          ? exif.ModifyDate.toISOString()
+          : new Date().toISOString()
+        : new Date().toISOString();
+      try {
+        let imageMediaInfo = {
+          flash: typeof exif.Flash == "number" ? exif.Flash : null,
+          fnumber: exif.FNumber ? String(exif.FNumber) : null,
+          color_space: exif.ColorSpace,
+          image_width: exif.ExifImageWidth,
+          focal_length: exif.FocalLength ? exif.FocalLength : null,
+          image_length: exif.ExifImageHeight,
+          light_source:
+            typeof exif.LightSource == "number" ? exif.LightSource : null,
+          exposure_mode:
+            typeof exif.ExposureMode == "number" ? exif.ExposureMode : null,
+          exposure_time: exif.ExposureTime
+            ? "1/" + String(Math.ceil(1 / exif.ExposureTime))
+            : null,
+          aperture_value: exif.ApertureValue
+            ? String(exif.ApertureValue)
+            : null,
+          datetime_original: date,
+          iso_speed_ratings: exif.ISO ? exif.ISO : null,
+          datetime_digitized: date,
+          max_aperture_value: exif.ApertureValue
+            ? String(exif.ApertureValue)
+            : null,
+          shutter_speed_value: exif.ShutterSpeedValue
+            ? String(exif.ShutterSpeedValue)
+            : null,
+        };
+        Object.keys(imageMediaInfo).forEach(
+          (key) => imageMediaInfo[key] == null && delete imageMediaInfo[key]
+        );
+        return [imageMediaInfo, date, device];
+      } catch {
+        return [{}, date, device];
+      }
+    } else if (file.type.includes("video")) {
+      const mediaInfo = await getInfo(file);
+      try {
+        let metadata = mediaInfo.media.track;
+        let videoMediaInfo = {
+          audio: {
+            channel: metadata[2].Channels,
+            bit_rate: parseFloat(metadata[2].BitRate),
+            sample_rate: parseInt(metadata[2].SamplingRate),
+            bits_per_sample: parseInt(metadata[2].BitDepth),
+            compression_rate: parseFloat(metadata[2].CodecID),
+          },
+          video: {
+            width: parseInt(metadata[1].Width),
+            height: parseInt(metadata[1].Height),
+            duration: parseInt(metadata[1].Duration),
+            frame_rate: parseFloat(metadata[1].FrameRate),
+            compression: metadata[1].CodecID,
+            bits_per_pixel: parseInt(metadata[1].BitDepth),
+          },
+          common: {
+            width: parseInt(metadata[1].Width),
+            height: parseInt(metadata[1].Height),
+            bit_rate: parseFloat(metadata[1].BitRate),
+            duration: parseInt(metadata[0].Duration),
+            mime_type: file.type,
+            endianness: metadata[2].Format_Settings_Endianess,
+            frame_rate: parseFloat(metadata[0].FrameRate),
+          },
+        };
+        return videoMediaInfo;
+      } catch {
+        return {};
+      }
+    }
+  },
+};
--- selia_uploader/views.py
+++ selia_uploader/views.py
@@ -1,5 +1,114 @@
+import json
+
+from django.http import Http404
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
+
+from irekua_items.models import Licence
+from irekua_items.models import ItemType
+from irekua_collections.models import Collection
+from irekua_collections.models import CollectionSite
+from irekua_collections.models import CollectionDevice
+from irekua_collections.models import SamplingEvent
+from irekua_collections.models import Deployment
+
+
+def _extract_argument_from_request(request, name, Model, required=False):
+    try:
+        pk = request.GET[name]
+        return Model.objects.get(pk=pk)
+
+    except KeyError:
+        if not required:
+            return None
+
+        raise Http404(f"No {name} was provided")
+
+    except Model.DoesNotExist:
+        raise Http404(f"{name.title()} does not exist")
 
 
 class UploadAppView(TemplateView):
     template_name = "selia_uploader/upload.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.arguments = self.extract_arguments(request)
+        return super().dispatch(request, *args, **kwargs)
+
+    def extract_arguments(self, request):
+        return {
+            "collection": _extract_argument_from_request(
+                request,
+                "collection",
+                Collection,
+                required=True,
+            ),
+            "licence": _extract_argument_from_request(
+                request,
+                "licence",
+                Licence,
+                required=True,
+            ),
+            "item_type": _extract_argument_from_request(
+                request,
+                "item_type",
+                ItemType,
+                required=True,
+            ),
+            "collection_site": _extract_argument_from_request(
+                request,
+                "collection_site",
+                CollectionSite,
+            ),
+            "collection_device": _extract_argument_from_request(
+                request,
+                "collection_device",
+                CollectionDevice,
+            ),
+            "sampling_event": _extract_argument_from_request(
+                request,
+                "sampling_event",
+                SamplingEvent,
+            ),
+            "deployment": _extract_argument_from_request(
+                request,
+                "deployment",
+                Deployment,
+            ),
+        }
+
+    def get(self, request):
+        items = {}
+        for i in request.GET:
+            if "deployment" in i:
+                dep = Deployment.objects.get(id=request.GET[i])
+                items["deployment"] = [dep.pk, dep.deployment_type.name]
+                items["collection_metadata"] = dep.collection_metadata
+                items["sampling_event"] = [
+                    dep.sampling_event.pk,
+                    dep.sampling_event.sampling_event_type.name,
+                ]
+                items["collection_site"] = [
+                    dep.sampling_event.collection_site.pk,
+                    dep.sampling_event.collection_site.collection_name,
+                ]
+                items["collection_device"] = [
+                    dep.collection_device.pk,
+                    dep.collection_device.collection_metadata["Nombres originales"],
+                ]
+                items["collection"] = [
+                    dep.collection_device.collection.pk,
+                    dep.collection_device.collection.name,
+                ]
+            elif "licence" in i:
+                lic = Licence.objects.get(id=request.GET[i])
+                items["licence"] = [lic.pk, lic.licence_type.name]
+            elif "item_type" in i:
+                mime = ItemType.objects.get(id=request.GET[i])
+                items["item_type"] = [mime.pk, mime.name]
+                items["mime_type"] = [
+                    mime.mime_types.all()[0].pk,
+                    mime.mime_types.all()[0].mime_type,
+                ]
+        return render(request, self.template_name, {"args": json.dumps(items)})
