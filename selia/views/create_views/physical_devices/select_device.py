c5553af5bef7f02c4635d24d82597bbf5b3a4797
jdonlucas <jdonlucas93@gmail.com>
2021-04-27 17:14:16 -0500

    COLLECTION_DEVICES: fix and create physical_devices view

--- selia/views/create_views/physical_devices/select_device.py
+++ selia/views/create_views/physical_devices/select_device.py
@@ -3,74 +3,67 @@ from django import forms
 from django.shortcuts import redirect
 from django.shortcuts import reverse
 from django.contrib.auth.models import Permission
-from dal import autocomplete
+from irekua_database.autocomplete import get_autocomplete_widget
 
 from irekua_devices.models import Device
 from irekua_devices.models import DeviceBrand
 from irekua_devices.models import DeviceType
+from irekua_schemas.models import Schema
+from irekua_collections.models import Collection
+from irekua_devices.filters import devices as devices_filters
 
 from selia.views.create_views.create_base import SeliaCreateView
 from selia_templates.forms.type_field import TypeSelectField
-from irekua_permissions.devices import (
-    physical_devices as device_permissions)
+from irekua_permissions.devices import physical_devices as device_permissions
+from selia.views.utils import SeliaList
 
 
-class SelectDeviceForm(forms.Form):
-    device = forms.ModelChoiceField(
-        required=False,
-        queryset=Device.objects.all(),
-        widget=autocomplete.ModelSelect2(
-            url='irekua_autocomplete:devices_autocomplete',
-            attrs={'style': 'width: 100%'}))
-    device_type = TypeSelectField(
-        required=False,
-        queryset=DeviceType.objects.all())
-    brand = forms.ModelChoiceField(
-        required=False,
-        queryset=DeviceBrand.objects.all(),
-        widget=autocomplete.ModelSelect2(
-            url='irekua_autocomplete:device_brands_autocomplete',
-            attrs={'style': 'width: 100%'}))
-    model = forms.CharField(required=False)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        device = cleaned_data.get("device", None)
-
-        if not device:
-            device_type = cleaned_data.get("device_type", None)
-            if device_type is None:
-                msg = 'No device type was assigned'
-                self.add_error('device_type', msg)
-
-            brand = cleaned_data.get("brand", None)
-            if brand is None:
-                msg = 'No device brand was assigned'
-                self.add_error('brand', msg)
-
-            model = cleaned_data.get("model", None)
-            if model is None:
-                msg = 'No device model was assigned'
-                self.add_error('model', msg)
 
 
 class SelectPhysicalDeviceDeviceView(SeliaCreateView):
-    form_class = SelectDeviceForm
     template_name = 'selia/create/physical_devices/select_device.html'
+    prefix = "device"
+    create_url = "selia:create_physical_device"
+    model = Device
+
+    def get_form_class(self):
+        class SelectDeviceForm(forms.ModelForm):
+            class Meta:
+                model = Device
+                fields = [
+                    "device_type",
+                    "brand",
+                    "model",
+                    "configuration_schema" 
+                ]
+
+                widgets = {
+                    "device_type": get_autocomplete_widget(model=DeviceType),
+                    "brand": get_autocomplete_widget(model=DeviceBrand),
+                    "configuration_schema": get_autocomplete_widget(model=Schema)
+                }
+
+        return SelectDeviceForm
+
 
     def has_view_permission(self):
         user = self.request.user
         return device_permissions.create(user)
 
-    def get(self, *args, **kwargs):
-        permission = Permission.objects.get(name='Can add Device Brand')
-        self.request.user.user_permissions.add(permission)
-        return super().get(*args, **kwargs)
+    def get_objects(self):
+        if not hasattr(self, "collection"):
+            self.collection = Collection.objects.get(
+                name=self.request.GET["collection"]
+            )
 
-    def get_form_kwargs(self, *args, **kwargs):
-        form_kwargs = super().get_form_kwargs(*args, **kwargs)
-        form_kwargs.pop('instance')
-        return form_kwargs
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, *kwargs)
+        context["collection"] = self.collection
+        context["list"] = self.get_device_list()
+        context["prefix"] = self.prefix
+        context["create_url"] = self.create_url
+
+        return context
 
     def redirect_on_success(self):
         url = reverse('selia:create_physical_device')
@@ -80,12 +73,21 @@ class SelectPhysicalDeviceDeviceView(SeliaCreateView):
         full_url = '{url}?{query}'.format(url=url, query=query.urlencode())
         return redirect(full_url)
 
-    def save_form(self, form):
-        device = form.cleaned_data['device']
 
-        if device is not None:
-            return device
+    def get_device_list(self):
+        devices = Device.objects.all()
 
-        data = form.cleaned_data.copy()
-        data.pop('device')
-        return Device.objects.create(**data)
+        class DeviceList(SeliaList):
+            prefix = "devices"
+
+            filter_class = devices_filters.Filter
+            search_fields = devices_filters.search_fields
+            ordering_fields = devices_filters.ordering_fields
+
+            queryset = devices
+
+            list_item_template = "selia/select_list_items/devices.html"
+            filter_form_template = "selia/filters/device.html"
+
+        device_list = DeviceList(self.request)
+        return device_list.get_context_data()
